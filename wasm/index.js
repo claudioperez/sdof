8e828ff3d2e77bdff6a43fe058e7d3ea15c31f9a
claudio perez <50180406+claudioperez@users.noreply.github.com>
2023-02-10 11:38:27 -0800

    cmp

--- wasm/index.js
+++ wasm/index.js
@@ -23,6 +23,38 @@ var f  =[
          0.0000];
 
 
+function Newmark(M,C,K,U0,V0,dt,Pt, gamma, beta) {
+  var n = f.length;
+
+  var _f = new Float64Array(f);
+  // change the view to Uint8, there isnt an ABI for
+  // c double arrays
+  var p = new Uint8Array(_f.buffer);
+
+  // TODO: FREE!!!
+  var ptr = Module._malloc(_f.byteLength*n*3);
+
+  var u = new Float64Array(n);
+  var v = new Float64Array(n);
+  var a = new Float64Array(n);
+
+  // Initialize displacement and velocity
+  Module.setValue(ptr+0*_f.BYTES_PER_ELEMENT,  U0, 'double');
+  Module.setValue(ptr+1*_f.BYTES_PER_ELEMENT,  V0, 'double');
+  Module.setValue(ptr+2*_f.BYTES_PER_ELEMENT, 0.0, 'double');
+
+  fsdof(0,M,C,K, 1.0,n,p,dt, ptr);
+
+  for (var i=0; i<n; i++)
+  {
+     u[i] = Module.getValue(ptr+(3*i+0)*_f.BYTES_PER_ELEMENT, 'double');
+     // v[i] = Module.getValue(ptr+(3*i+1)*_f.BYTES_PER_ELEMENT, 'double');
+     // a[i] = Module.getValue(ptr+(3*i+2)*_f.BYTES_PER_ELEMENT, 'double');
+  }
+  return u;
+  // return {u: u, v: v, a: a};
+}
+
 function RunFSDOF(M,C,K, f,dt) {
   var n = f.length;
 
