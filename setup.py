fd51c3253d0b4a78d36435beb3da21607ff3d4f6
claudio perez <50180406+claudioperez@users.noreply.github.com>
2023-02-10 09:59:59 -0800

    cmp - cleaning

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ from setuptools import Extension, setup
 setup(
     ext_modules=[
         Extension(
-            name="sdof._fsdof",  # as it would be imported
-                                 # may include packages/namespaces separated by `.`
-
-            sources=["src/fsdof.c"], # all sources are compiled into a single binary file
+            name="sdof._fsdof",
+            sources=["src/fsdof.c"],
             export_symbols=[]
         ),
     ]
